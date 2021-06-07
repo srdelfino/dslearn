048f5a82b6ecbb6fa2dcf2d17f730924c4ee2b88
Sérgio Roberto Delfino <srdelfino@gmail.com>
2021-06-07 15:22:30 -0300

    04-04

---
+++ backend/src/main/java/br/pro/delfino/dslearn/DSLearnApplication.java
@@ -0,0 +1,13 @@
+package br.pro.delfino.dslearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DSLearnApplication.class, args);
+    }
+
+}
